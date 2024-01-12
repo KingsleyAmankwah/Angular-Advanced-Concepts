f4da49dffd9546522c6640d3d5380fd9a9cbb498
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-01-12 10:58:36 -0800

    routes added

--- chip-project/src/app/app.routes.ts
+++ chip-project/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ChipComponent } from './chip/chip.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ChipComponent, title: 'Home' },
+];
